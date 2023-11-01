01ccb710e645bdd11c7b08c32cfb1c41d8d68d46
Ivan Diaz <ivandiaz94ing@gmail.com>
2023-11-01 12:16:13 -0500

    first commit intro a typescript

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import './style.css' 
+//import './topics/01-type-basic'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+ Hola Mundo con Typescript
+`
+console.log('Hola mundo desde la consola de typescript');
+
